75ddce3cc369f8f012e3ca3e9597431f874917fa
Hector Mosquera <hectormosq@gmail.com>
2025-01-29 19:12:33 +0100

    WIP: setting base header for layout

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 export default function Header() {
-    return(
-        <header>
-            
-        </header>
-    )
+  return (
+    <header>
+      <nav>
+        <ul >
+          <li>
+            <Link href="/">Main</Link>
+          </li>
+          <li>
+            <Link href="/admin">Add</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import MovementList from "./MovementList";
-import Header from "./components/Header";
 
 export default function Home() {
   return (
-    <div>
-      <Header></Header>
+    <>
       <MovementList></MovementList>
-    </div>
+    </>
   );
 }
