8f304981cc30d4e9f427631c2607db9b7a8e293a
Hector Mosquera <hectormosq@gmail.com>
2025-02-15 14:04:13 +0100

    Configuring testing

---
+++ client/__test__/page.test.tsx
@@ -0,0 +1,9 @@
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import Home from "@/app/page";
+
+describe("Home", () => {
+  it("should render the home page", () => {
+    render(<Home />);
+  });
+});
