e2576fe110c44e1df16dd632852e2103afc43d10
Hector Mosquera <hectormosq@gmail.com>
2025-03-04 13:39:22 +0100

    feat/3-setup-styled-component

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      // Rewrites all API requests to express server
-      {
-        source: "/api/v1/:path*",
-        destination: "http://localhost:5000/api/v1/:path*",
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
