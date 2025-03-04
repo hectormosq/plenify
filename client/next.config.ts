891ea64907716b2f5c4b9e8b178b9f55595c212d
Hector Mosquera <hectormosq@gmail.com>
2025-03-04 13:48:08 +0100

    Undo changes for next config

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      // Rewrites all API requests to express server
+      {
+        source: "/api/v1/:path*",
+        destination: "http://localhost:5000/api/v1/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
