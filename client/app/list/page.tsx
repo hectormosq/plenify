3e7eaa38e380452829780d1ad055b6ff4f781585
Hector Mosquera <hectormosq@gmail.com>
2025-03-18 17:51:12 +0100

    Refactoring

--- client/app/list/page.tsx
+++ client/app/list/page.tsx
@@ -1,11 +1,42 @@
 "use client";
 
-import MovementList from "../MovementList";
+import Loader from "../components/loader";
+import TransactionList from "../components/transactions/transaction-list";
+import { usePlenifyState } from "../hooks/usePlenifyState";
 
-export default function TransactionList() {
+
+export default function TransactionListPage() {
+  const { loading, transactions, addTransaction, reset } = usePlenifyState();
+
   return (
     <>
-      <MovementList></MovementList>
+      {loading ? (
+        <Loader />
+      ) : (
+        <TransactionList transactionList={transactions} />
+      )}
+      <div style={{ display: "flex", gap: "10px" }}>
+        <button
+          style={{
+            padding: ".75rem 2rem",
+            background: "grey",
+            margin: "10px 0",
+          }}
+          onClick={addTransaction}
+        >
+          Add Transaction
+        </button>
+        <button
+          style={{
+            padding: ".75rem 2rem",
+            background: "grey",
+            margin: "10px 0",
+          }}
+          onClick={reset}
+        >
+          Delete data
+        </button>
+      </div>
     </>
   );
 }
