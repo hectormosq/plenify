14515190b530dea85827fcc0ede0d15900a30ce9
Hector Mosquera <hectormosq@gmail.com>
2025-03-24 17:36:55 +0100

    Adding tabs

--- client/app/dashboard/page.tsx
+++ client/app/dashboard/page.tsx
@@ -6,8 +6,31 @@ import Loader from "../components/loader";
 import TransactionList from "../components/transactions/transaction-list";
 import { PieChart } from "@mui/x-charts/PieChart";
 import ChartService from "../services/chart";
+import { Tab, Tabs } from "@mui/material";
+import { useState } from "react";
+import Link from "next/link";
+
+const tabProps = (index: number) => {
+  return {
+    id: `action-tab-${index}`,
+    "aria-controls": `action-tabpanel-${index}`,
+  };
+};
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index } = props;
+
+  return <>{value === index && <div>{children}</div>}</>;
+}
 
 export default function DashboardPage() {
+  const [activeTab, setActiveTab] = useState(0);
   const { loading, transactions, categories } = usePlenifyState();
   // TODO fix categories to be defined
 
@@ -26,38 +49,64 @@ export default function DashboardPage() {
             <p>Summary</p>
           </div>
           <div className={classes.summaryChart}>
-            {series && series.length > 0 ? (
-              <PieChart
-                series={[
-                  {
-                    innerRadius: 115,
-                    outerRadius: 70,
-                    paddingAngle: 1.5,
-                    cornerRadius: 2,
-                    data: series[0],
-                  },
-                  {
-                    innerRadius: 120,
-                    outerRadius: 150,
-                    paddingAngle: 1.25,
-                    cornerRadius: 2,
-                    data: series[1],
-                  },
-                ]}
-                width={400}
-                height={300}
-                slotProps={{
-                  legend: { hidden: true },
-                }}
+            <Tabs
+              value={activeTab}
+              onChange={(_: unknown, newValue: number) => {
+                setActiveTab(newValue);
+              }}
+            >
+              <Tab
+                style={{ color: activeTab === 0 ? "#9ACD32" : "#ddd6cb" }}
+                label="Expenses"
+                {...tabProps(0)}
               />
-            ) : (
-              <div>No data to display</div>
-            )}
+              <Tab
+                style={{ color: activeTab === 1 ? "#9ACD32" : "#ddd6cb" }}
+                label="Incomes"
+                {...tabProps(1)}
+              />
+            </Tabs>
+
+
+            <TabPanel value={activeTab} index={0}>
+              <>
+                {series && series.length > 0 ? (
+                  <PieChart
+                    series={[
+                      {
+                        innerRadius: 115,
+                        outerRadius: 70,
+                        paddingAngle: 1.5,
+                        cornerRadius: 2,
+                        data: series[0],
+                      },
+                      {
+                        innerRadius: 120,
+                        outerRadius: 150,
+                        paddingAngle: 1.25,
+                        cornerRadius: 2,
+                        data: series[1],
+                      },
+                    ]}
+                    width={400}
+                    height={300}
+                    slotProps={{
+                      legend: { hidden: true },
+                    }}
+                  />
+                ) : (
+                  <div>No data to display</div>
+                )}
+              </>
+            </TabPanel>
+            <TabPanel value={activeTab} index={1}>
+              <>Incomes</>
+            </TabPanel>
           </div>
           <div>
             <div className={classes.transactionsHeader}>
               <div>Recent Transactions</div>
-              <div>See All</div>
+              <div><Link href="/transaction-list">See All</Link></div>
             </div>
             <TransactionList transactionList={recentTransactions} />
           </div>
